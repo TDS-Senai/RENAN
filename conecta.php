ce3545edd03fcb98baf3a23f56099501a6724bcc
TCC <132756039+TDS-Senai@users.noreply.github.com>
2023-05-12 22:35:39 -0300

    Delete conecta.php

--- conecta.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","","revisao");
-    mysqli_set_charset($conn, "utf8");
-    if (!$conn) 
-    {
-        echo "Falha ao conectar-se ao Database, verifique!!".PHP_EOL;
-        echo "Erro: ".mysqli_connect_error().PHP_EOL;
-    }
-?>
